a55a3425bbd6ac0078692ebbe862b72bffbcacc0
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-04-03 21:38:18 +0300

    reformat comment

--- Setup-hs-main/Lecture2/HelloWorld.hs
+++ Setup-hs-main/Lecture2/HelloWorld.hs
@@ -1,6 +1,6 @@
 module Main where
 
--- namespacing Mystrings with Lib since the Library.hs is inside Lib/ folder
+-- namespacing Mystrings with Lib since it is inside the Lib/ folder
 import Lib.MyStrings (greeting)
 
 main :: IO ()
